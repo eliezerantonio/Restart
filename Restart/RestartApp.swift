b58ad0621870b075e4e754c189600e40525c9541
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-10-11 21:19:43 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Eliezer Antonio on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
